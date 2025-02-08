d1d8ec5f0eda2fbd0ca8ed36c5aaeaf8ff298da9
monarche-harsha <vardhan17harsha@gmail.com>
2025-02-09 00:44:55 +0530

    added seach bar n mockdata and integrated it

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,20 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 
-const Dashboard = () => {
-  return <Layout>Dashboard page</Layout>;
+const Dashboard = ({ selectedCompany }) => {
+  return (
+    <Layout>
+      <h2>Dashboard</h2>
+      {selectedCompany ? (
+        <div>
+          <h3>{selectedCompany.company_name}</h3>
+          <p>Job Role: {selectedCompany.job_role}</p>
+        </div>
+      ) : (
+        <p>Please select a company to view job roles.</p>
+      )}
+    </Layout>
+  );
 };
 
 export default Dashboard;
